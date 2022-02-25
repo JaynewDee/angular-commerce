eef0755cd8a668cd46e9c2d419f5ac40038c9268
NuminousBlue <jdiehl2236@gmail.com>
2022-02-25 16:36:05 -0600

    initialized fundamental components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule
